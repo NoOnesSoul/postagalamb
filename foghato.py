0a2e44694faf88802d479823b91650d14d890959
NoOnesSoul <98747653+NoOnesSoul@users.noreply.github.com>
2022-06-02 12:06:22 +0200

    fish parancshoz

---
+++ foghato.py
@@ -0,0 +1,15 @@
+foghatosag = [
+    "common_fish", "common_fish", "common_fish", "common_fish", "common_fish", "common_fish",
+    "trash", "trash", "trash", "trash",
+    "nothing", "nothing", "nothing",
+    "rare_fish",
+    "treasure"
+]
+
+rare_fish = [
+    "gold_fish", "puffer_fish", "baby_shark"
+]
+
+treasure = [
+    100000, 50000, 10000, 5000, 1000, 500, 100, 50  
+]
